8c381327d7207d2ade587579ea60352a5e29c716
AyaneSuto <129638089+TsuKi-3Moti@users.noreply.github.com>
2023-04-12 12:41:52 +0000

    [Finish] 他ユーザーからのアクセス制限の更新

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update]
+
   def show
     @user = User.find(params[:id])
     @post_images = @user.post_images.page(params[:page]) #特定のユーザ(@user)に関連付けられた投稿の１ページ分(.post_images.page(params[:page]))を取得し@post_imagesに渡す
@@ -16,7 +18,15 @@ class UsersController < ApplicationController
 
   private
 
-  def user_params
-    params.require(:user).permit(:name, :profile_image)
+    def user_params
+      params.require(:user).permit(:name, :profile_image)
+    end
+
+    def is_matching_login_user
+      user = User.find(params[:id])
+      unless user.id == current_user.id
+        redirect_to post_images_path
+      end
+    end
+
   end
-end
