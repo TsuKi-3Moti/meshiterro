b09e285c464538c560a528ef0bb7ac1dee9b924e
AyaneSuto <129638089+TsuKi-3Moti@users.noreply.github.com>
2023-04-12 08:51:33 +0000

    [Fix] bootstrapの修正

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -7,7 +7,7 @@ environment.plugins.prepend(
   'Provide',
   new webpack.ProvidePlugin({
     $: 'jquery/src/jquery',
-    jQuery: 'jpuery/src/jquery',
+    jQuery: 'jquery/src/jquery',
     Popper: 'popper.js'
   })
 )
